57bc25d81fc626bceed4e58fb28e5a88dd0c77b1
gabmeloo <gabrielademelo.9c@gmail.com>
2023-11-29 16:50:58 -0300

    AXIOS

--- src/app/page.js
+++ src/app/page.js
@@ -1,46 +1,56 @@
 "use client"
-import Card from '@/components/card'
-import Titulo from '@/components/titulo'
-import Image from 'next/image'
-import { useState } from 'react';
+
+import Card from './components/card';
+import Titulo from './components/titulo';
+import Image from 'next/image';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function Home() {
-  const [filmes, setFilmes] = useState([])
+  const [filmes, setFilmes] = useState([]);
 
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZTkyMjY2NzQ4MWFiMjA3ZDY0MjQ1MGIwZWZiNDYxZSIsInN1YiI6IjVlYTA5ZTZiYmU0YjM2MDAxYzU5NWExNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Vhu0pPCiIwmtrpyOHdBlQid8HJJllaHthn1MERS_ANg'
+  useEffect(() => {
+    // Função assíncrona para obter filmes até 12 anos
+    async function fetchMovies() {
+      try {
+        const options = {
+          method: 'GET',
+          headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZTkyMjY2NzQ4MWFiMjA3ZDY0MjQ1MGIwZWZiNDYxZSIsInN1YiI6IjVlYTA5ZTZiYmU0YjM2MDAxYzU5NWExNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Vhu0pPCiIwmtrpyOHdBlQid8HJJllaHthn1MERS_ANg'
+          }
+        };
+
+        const response = await axios.get('https://api.themoviedb.org/3/trending/movie/week?language=pt-BR', options);
+        setFilmes(response.data.results);
+      } catch (error) {
+        console.error('Erro ao obter filmes:', error.message);
+      }
     }
-  };
-  
-  fetch('https://api.themoviedb.org/3/trending/movie/week?language=pt-BR', options)
-    .then(response => response.json())
-    .then(response => setFilmes(response.results))
-    .catch(err => console.error(err));
+
+    // Chamando a função de busca de filmes ao carregar o componente
+    fetchMovies();
+  }, []); // O segundo argumento vazio significa que essa função será executada apenas uma vez, equivalente a componentDidMount em classes
 
   return (
     <main className="flex min-h-screen flex-col">
-      <nav className="flex gap-4 justify-between items-end bg-slate-700 w-full p-3">
-        <h1 className='flex items-center gap-2 text-5xl text-amber-400 font-bold uppercase'>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-12 h-12">
-          <path fillRule="evenodd" d="M1.5 5.625c0-1.036.84-1.875 1.875-1.875h17.25c1.035 0 1.875.84 1.875 1.875v12.75c0 1.035-.84 1.875-1.875 1.875H3.375A1.875 1.875 0 011.5 18.375V5.625zm1.5 0v1.5c0 .207.168.375.375.375h1.5a.375.375 0 00.375-.375v-1.5a.375.375 0 00-.375-.375h-1.5A.375.375 0 003 5.625zm16.125-.375a.375.375 0 00-.375.375v1.5c0 .207.168.375.375.375h1.5A.375.375 0 0021 7.125v-1.5a.375.375 0 00-.375-.375h-1.5zM21 9.375A.375.375 0 0020.625 9h-1.5a.375.375 0 00-.375.375v1.5c0 .207.168.375.375.375h1.5a.375.375 0 00.375-.375v-1.5zm0 3.75a.375.375 0 00-.375-.375h-1.5a.375.375 0 00-.375.375v1.5c0 .207.168.375.375.375h1.5a.375.375 0 00.375-.375v-1.5zm0 3.75a.375.375 0 00-.375-.375h-1.5a.375.375 0 00-.375.375v1.5c0 .207.168.375.375.375h1.5a.375.375 0 00.375-.375v-1.5zM4.875 18.75a.375.375 0 00.375-.375v-1.5a.375.375 0 00-.375-.375h-1.5a.375.375 0 00-.375.375v1.5c0 .207.168.375.375.375h1.5zM3.375 15h1.5a.375.375 0 00.375-.375v-1.5a.375.375 0 00-.375-.375h-1.5a.375.375 0 00-.375.375v1.5c0 .207.168.375.375.375zm0-3.75h1.5a.375.375 0 00.375-.375v-1.5A.375.375 0 004.875 9h-1.5A.375.375 0 003 9.375v1.5c0 .207.168.375.375.375zm4.125 0a.75.75 0 000 1.5h9a.75.75 0 000-1.5h-9z" clipRule="evenodd" />
-        </svg>
-          Senac Filmes
+      <nav className="flex gap-4 justify-between items-end bg-purple-950 w-full p-3">
+        <h1 className='flex items-center gap-2 text-5xl text-white font-bold uppercase'>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-12 h-12">
+            <path fillRule="evenodd" d="M1.5 5.625c0-1.036.84-1.875 1.875-1.875h17.25c1.035 0 1.875.84 1.875 1.875v12.75c0 1.035-.84 1.875-1.875 1.875H3.375A1.875 1.875 0 011.5 18.375V5.625zm1.5 0v1.5c0 .207.168.375.375.375h1.5a.375.375 0 00.375-.375v-1.5a.375.375 0 00-.375-.375h-1.5A.375.375 0 003 5.625zm16.125-.375a.375.375 0 00-.375.375v1.5c0 .207.168.375.375.375h1.5A.375.375 0 0021 7.125v-1.5a.375.375 0 00-.375-.375h-1.5zM21 9.375A.375.375 0 0020.625 9h-1.5a.375.375 0 00-.375.375v1.5c0 .207.168.375.375.375h1.5a.375.375 0 00.375-.375v-1.5zm0 3.75a.375.375 0 00-.375-.375h-1.5a.375.375 0 00-.375.375v1.5c0 .207.168.375.375.375h1.5a.375.375 0 00.375-.375v-1.5zm0 3.75a.375.375 0 00-.375-.375h-1.5a.375.375 0 00-.375.375v1.5c0 .207.168.375.375.375h1.5a.375.375 0 00.375-.375v-1.5zM4.875 18.75a.375.375 0 00.375-.375v-1.5a.375.375 0 00-.375-.375h-1.5a.375.375 0 00-.375.375v1.5c0 .207.168.375.375.375h1.5zM3.375 15h1.5a.375.375 0 00.375-.375v-1.5a.375.375 0 00-.375-.375h-1.5a.375.375 0 00-.375.375v1.5c0 .207.168.375.375.375zm0-3.75h1.5a.375.375 0 00.375-.375v-1.5A.375.375 0 004.875 9h-1.5A.375.375 0 003 9.375v1.5c0 .207.168.375.375.375zm4.125 0a.75.75 0 000 1.5h9a.75.75 0 000-1.5h-9z" clipRule="evenodd" />
+          </svg>
+          Nostalgic Movie
         </h1>
         <a href="/sobre">Sobre</a>
       </nav>
 
-      <Titulo>filmes em alta</Titulo>
+      <Titulo>Os mais procurados</Titulo>
       <section className='flex flex-wrap gap-4 p-4'>
-        {filmes && filmes.map(filme => <Card filme={filme} /> ) }
-        
+        {filmes && filmes.map(filme => <Card filme={filme} key={filme.id} />)}
       </section>
 
-      <Titulo>séries em alta</Titulo>
-      <Titulo>lançamentos</Titulo>
-      
+      <Titulo>2000...</Titulo>
+      <Titulo>1990...</Titulo>
     </main>
-  )
+  );
 }
